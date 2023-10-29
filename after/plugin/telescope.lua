d912bb03fbfd0c095858e32b1902dc7ec488cfff
James-Steve <hamsterofgold@gmail.com>
2023-10-29 13:16:53 +0200

    Neovim telescope added the ability to insert the file path

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,12 +1,28 @@
 local tel = require('telescope')
+local path_actions = require('telescope_insert_path')
+
 tel.setup {
     defaults = {
+        mappings = {
+      n = {
+        -- E.g. Type `[i`, `[I`, `[a`, `[A`, `[o`, `[O` to insert relative path and select the path in visual mode.
+        -- Other mappings work the same way with a different prefix.
+        --["["] = path_actions.insert_reltobufpath_visual,
+        --["]"] = path_actions.insert_abspath_visual,
+        --["{"] = path_actions.insert_reltobufpath_insert,
+        --["}"] = path_actions.insert_abspath_insert,
+        ["pr"] = path_actions.insert_reltobufpath_normal,
+        ["pa"] = path_actions.insert_abspath_normal,
+	-- If you want to get relative path that is relative to the cwd, use
+	-- `relpath` instead of `reltobufpath`
+        -- You can skip the location postfix if you specify that in the function name.
+        -- ["<C-o>"] = path_actions.insert_relpath_o_visual,
+      }
+    },
         file_ignore_patterns = { ".git/objects", ".git/heads", ".git/hooks", ".git/index", ".git/*HEAD*", ".git/logs", ".git/*refs*", ".git/info", ".git/packed*", ".git/COMMITS*",
             ".git/description", ".git/ORIG*", ".git/FETCH*" }
-
     }
 }
-
 local opts = {
     file_ignore_patterns = { ".git/", ".git/*" }
 }
