5ef71c9943b303a9d323d8fdae07a9da0f44eb1b
James-Steve <hamsterofgold@gmail.com>
2024-08-27 10:52:13 +0200

    disbaled magma

--- after/plugin/magma.lua
+++ after/plugin/magma.lua
@@ -1,9 +1,11 @@
+--[[
 local opts = {buffer = bfnr, remap = false}
 vim.api.nvim_create_autocmd('BufEnter',
 {
     pattern = {"*.py"},
     group = group,
     callback = function(env)
+        --[[
         vim.keymap.set('n', "Tk", function() vim.cmd "vim_exec('MagmaEvaluateOperator', v:true)" end, opts)
         vim.keymap.set('n', "TK", function() vim.cmd "MagmaEvaluateLine" end, opts)
         vim.keymap.set('n', "Tv", function() vim.cmd "MagmaEvaluateVisual" end, opts)
@@ -13,6 +15,6 @@ vim.api.nvim_create_autocmd('BufEnter',
         vim.cmd[[
     :MagmaInit python3
     :MagmaEvaluateArgument a=5
-    ]]
 end
 })
+--]]
