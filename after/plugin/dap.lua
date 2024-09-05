2bd9460e382db24994e833a0372e8b67dee968c3
James-Steve <hamsterofgold@gmail.com>
2024-09-05 13:01:34 +0200

    neovim dap ui changed sizes

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,5 +1,5 @@
 local dap, dapui = require("dap"), require("dapui")
---dap.setup()
+-- dap.setup()
 dapui.setup()
 dap.listeners.before.attach.dapui_config = function() dapui.open() end
 dap.listeners.before.launch.dapui_config = function() dapui.open() end
@@ -33,8 +33,53 @@ vim.keymap.set("n", "<Leader>ds", function()
     widgets.centered_float(widgets.scopes)
 end)
 
+vim.keymap.set({"n", "v"}, "<Leader>de", function() require("dapui").eval() end)
 
-vim.keymap.set({"n", "v"}, "<Leader>de", function() 
-    require("dapui").eval()
-
-end)
+local sizeLeft = 60
+local sizeBottom = 10
+local config = {
+    controls = {
+        element = "repl",
+        enabled = true,
+        icons = {
+            disconnect = "",
+            pause = "",
+            play = "",
+            run_last = "",
+            step_back = "",
+            step_into = "",
+            step_out = "",
+            step_over = "",
+            terminate = ""
+        }
+    },
+    element_mappings = {},
+    expand_lines = true,
+    floating = {border = "single", mappings = {close = {"q", "<Esc>"}}},
+    force_buffers = true,
+    icons = {collapsed = "", current_frame = "", expanded = ""},
+    layouts = {
+        {
+            elements = {
+                {id = "scopes", size = 0.25}, {id = "breakpoints", size = 0.25},
+                {id = "stacks", size = 0.25}, {id = "watches", size = 0.25}
+            },
+            position = "left",
+            size = sizeLeft
+        }, {
+            elements = {{id = "repl", size = 0.5}, {id = "console", size = 0.5}},
+            position = "bottom",
+            size = 10
+        }
+    },
+    mappings = {
+        edit = "e",
+        expand = {"<CR>", "<2-LeftMouse>"},
+        open = "o",
+        remove = "d",
+        repl = "r",
+        toggle = "t"
+    },
+    render = {indent = 1, max_value_lines = 100}
+}
+dapui.setup(config)
