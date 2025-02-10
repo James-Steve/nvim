c80a2bd155ddb023188218e30c7472e9961a113b
James-Steve <hamsterofgold@gmail.com>
2025-02-10 13:10:13 +0200

    Neovim, dadbod (sql handler) add a dotenviroment file to handle connections

--- after/plugin/dadbod.lua
+++ after/plugin/dadbod.lua
@@ -1,3 +1,9 @@
 vim.g.db_ui_execute_on_save = 0
 vim.g.db_ui_save_location = 'Database'
 vim.g.db_ui_dotenv_variable_prefix = 'dbui'
+local dot = require("hamster.env")
+
+dot.load_dotenv(vim.fn.expand('$HOME/.config/env/dbs.env'))
+local name = dot.get("PVProjDevName","obama")
+local connection =  dot.get("PVProjDev","obama")
+vim.g.dbs = {{name = name, url = connection}}
