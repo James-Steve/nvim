c68b009de32a5c63dac72d523788cd9ef17420e0
James-Steve <hamsterofgold@gmail.com>
2025-06-17 20:56:54 +0200

    Neovim vimtex removed nonsense

--- after/plugin/vimtex.lua
+++ after/plugin/vimtex.lua
@@ -1,55 +1,10 @@
--- vimtext settings
---[[
-" This is necessary for VimTeX to load properly. The "indent" is optional.
-" Note that most plugin managers will do this automatically.
-filetype plugin indent on
-
-" This enables Vim's and neovim's syntax-related features. Without this, some
-" VimTeX features will not work (see ":help vimtex-requirements" for more
-" info).
-syntax enable
-
-" Viewer options: One may configure the viewer either by specifying a built-in
-" viewer method:
-let g:vimtex_view_method = 'zathura'
-
-" Or with a generic interface:
-let g:vimtex_view_general_viewer = 'okular'
-let g:vimtex_view_general_options = '--unique file:@pdf\#src:@line@tex'
-
-" VimTeX uses latexmk as the default compiler backend. If you use it, which is
-" strongly recommended, you probably don't need to configure anything. If you
-" want another compiler backend, you can change it as follows. The list of
-" supported backends and further explanation is provided in the documentation,
-" see ":help vimtex-compiler".
-let g:vimtex_compiler_method = 'latexrun'
-
-" Most VimTeX mappings rely on localleader and this can be changed with the
-" following line. The default is usually fine and is the symbol "\".
-let maplocalleader = ","
---]] -- vim.g.vimtex_view_method="zathura"
--- let g:livepreview_previewer = 'evince'
 vim.g.vimtex_view_method = "okular"
 vim.g.livepreview_previewer = "okular"
--- vim.g.vimtex_compiler_latexmk.aux_dir="aux/"
--- vim.g.vimtex_compiler_latexmk.out_dir="out/"
--- vim.g.vimtex_compiler_latexmk = {aux_dir="aux/", out_dir="out/"}
+
 --[[
-vim.g.vimtex_compiler_latexmk = {
-    aux_dir = 'Auxiliary',
-    out_dir = 'Output',
-    callback = 1,
-    continuous = 1,
-    executable = 'latexmk',
-    hooks = {},
-    options = {
-        '-verbose',
-        '-file-line-error',
-        '-synctex=1',
-        '-interaction=nonstopmode',
-    },
-}
---]]
+vim.g.vimtex_compiler_generic = {
+   command= 'ls @tex | entr -n -c tectonic /_ --synctex --keep-logs',}
+   --]]
 
 vim.g.vimtex_compiler_latexmk = {
     executable = 'latexmk',
@@ -61,20 +16,10 @@ vim.g.vimtex_compiler_latexmk = {
     }
 }
 vim.g.tq_mthesaur_file = "~/Documents/mthesaur.txt"
+
 --[[
-    let g:vimtex_compiler_latexmk = {
-        \ 'aux_dir' : '',
-        \ 'out_dir' : '',
-        \ 'callback' : 1,
-        \ 'continuous' : 1,
-        \ 'executable' : 'latexmk',
-        \ 'hooks' : [],
-        \ 'options' : [
-        \   '-verbose',
-        \   '-file-line-error',
-        \   '-synctex=1',
-        \   '-interaction=nonstopmode',
-        \ ],
-        \}
-        --]]
---
+Mappings
+<leader>ll start compiling in continous mode
+<leader>lt toggle navigation bar
+
+--]]
